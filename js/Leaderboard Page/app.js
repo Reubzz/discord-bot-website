8a77b0d9a80c9e6763f5f78a81605d02affea225
Reubz <reugeorac@gmail.com>
2022-06-17 23:19:53 +0530

    Visual Update 1.1

--- js/Leaderboard Page/app.js
+++ js/Leaderboard Page/app.js
@@ -1,5 +1,20 @@
-// function lbdata(){
-//     $.get('https://temp-api-endpoints.reubzz.repl.co/lb', (x) => {
-//         console.log(x)
-//     })
-// }
+// Search Function 
+function search(){
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('lb-data');
+    let elem = document.getElementById('lb-box');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+            // let result = []
+            // result =+ x[i]
+            // elem.innerHTML = result
+            // console.log(result)
+        }
+        else {
+            x[i].style.display="";                 
+        }
+    }
+}
