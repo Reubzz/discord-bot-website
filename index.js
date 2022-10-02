68c466643bcfeeaf7d5a7ef8f8daff57f125d896
Reubz <reugeorac@gmail.com>
2022-10-02 20:57:04 +0530

    Added new socials endpoints

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const apicache = require('apicache');
+const config = require('./config.json')
 
 var exphbs = require('express-handlebars');
 require('dotenv').config()
@@ -16,15 +17,30 @@ app.use(express.static('public'))
 app.use('/', require(path.join(__dirname, '/routes/leaderboard')))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'pages', 'home', 'home.html'))
+    res.sendFile(path.join(__dirname, 'pages', 'home', 'home2.html'))
 });
 
 app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, 'pages', 'home', 'home.html'))
+    res.sendFile(path.join(__dirname, 'pages', 'home', 'home2.html'))
 });
 app.get('/commands', (req, res) => {
     res.sendFile(path.join(__dirname, 'pages', 'commands', 'commands.html'))
 });
+app.get('/instagram', (req, res) => {
+    res.redirect(config.socials.instagram)
+})
+app.get('/twitter', (req, res) => {
+    res.redirect(config.socials.twitter)
+})
+app.get('/discord', (req, res) => {
+    res.redirect(config.socials.discord)
+})
+app.get('/email', (req, res) => {
+    res.redirect(config.socials.email)
+})
+app.get('/github', (req, res) => {
+    res.redirect(config.socials.github)
+})
 
 
 // Custom Error Page
@@ -32,4 +48,4 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'pages', 'Errors', '404.html'))
 })
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
